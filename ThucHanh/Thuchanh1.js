cc4124966570432e75a3f49804416fb74901eccd
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-07-07 21:27:33 +0700

    New

---
+++ ThucHanh/Thuchanh1.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+function ShowMessage(){
+    document.getElementById('massageDiv').innerHTML = 'Xin chào!';
+}
+function Show(){
+    document.write('Xin chào!');
+}
+function Message(){
+    console.log('Xin Chào!');
+}
