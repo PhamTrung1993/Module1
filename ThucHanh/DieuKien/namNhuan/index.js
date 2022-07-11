e8cb7b11a21ebef4a93f46be68ad2785165d2d23
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-07-11 20:29:35 +0700

    I

---
+++ ThucHanh/DieuKien/namNhuan/index.js
@@ -0,0 +1,13 @@
+let year = parseInt(prompt('Enter a year'));
+if (year % 4 == 0) {
+    if (year % 100 == 0){
+        if (year % 400 == 0) {
+            alert(year + " is a leap year");
+
+        }
+        else {alert(year + " is a NOT leap year");}
+
+    }
+    else {alert(year + " is a leap year");}
+}
+else { alert(year + "is a NOT leap year");}
