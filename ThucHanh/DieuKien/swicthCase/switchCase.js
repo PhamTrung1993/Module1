2496416705407119cce89d837cb8f252ed532382
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-07-11 21:20:17 +0700

    I

---
+++ ThucHanh/DieuKien/swicthCase/switchCase.js
@@ -0,0 +1,14 @@
+let browser = prompt("Enter browser name!");
+switch (browser) {
+    case 'Edge' :
+        alert("You've got the Edge!");
+        break;
+    case 'Chrome' :
+    case 'FireFox' :
+    case 'Safari' :
+    case 'Opera' :
+        alert('Okay we support these browsers too');
+        break;
+    default :
+        alert('We hope that this page look ok!');
+}
