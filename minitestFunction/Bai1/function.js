348a79c0e5fd396b6e5c9144cc432ce609c8408a
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-07-23 16:08:19 +0700

    MiniTestFBai1

---
+++ minitestFunction/Bai1/function.js
@@ -0,0 +1,42 @@
+let array = '';
+function importArray() {
+    let length = +prompt("import array length: ")
+    array = getArray(length);
+    document.getElementById("Array").innerHTML = array;
+
+}
+function showEven() {
+    let  result = checkEven(array);
+    document.getElementById("result").innerHTML = result;
+}
+function checkEven(arr) {
+    let count = 0;
+    let result = '';
+    let evenArray = new Array()
+    for (let i = 0 ; i < arr.length; i++) {
+        if (arr[i]%2 === 0) {
+            count = count +1;
+            evenArray.push(arr[i])
+        }
+    }
+    if (count === 0 ) {
+        result = 'Arrays without even numbers';
+    }
+    else {
+        result = 'Array with ' + count + ' even number is: ' + evenArray;
+    }
+    return result;
+}
+function getArray(length) {
+    let array = new Array(length);
+    for (let i = 0; i < array.length;i++) {
+        array[i] = +prompt("import value of " + (i+1));
+    }
+    return array;
+}
+function reset() {
+    document.getElementById("Array").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
+
+
+}
