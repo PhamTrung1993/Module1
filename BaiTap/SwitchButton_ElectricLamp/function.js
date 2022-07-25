be6a1e8aae38ba5dc331a57a664239cfaa659940
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-07-25 11:24:58 +0700

    SwitchLamp

---
+++ BaiTap/SwitchButton_ElectricLamp/function.js
@@ -0,0 +1,39 @@
+class ElectricLamp {
+    constructor() {
+        this.status = false;
+    }
+    turnOff() {
+        this.status = false;
+    }
+    turnOn() {
+        this.status = true
+    }
+}
+class SwitchButton{
+    constructor() {
+        this.status;
+        this.lamp;
+    }
+    connectToLamp(lamp) {
+        this.lamp = lamp
+    }
+
+    switchOff() {
+        this.lamp.turnOff();
+    }
+    switchOn() {
+        this.lamp.turnOn()
+    }
+}
+let electricLamp = new ElectricLamp();
+let switchButton = new SwitchButton();
+
+switchButton.connectToLamp(electricLamp);
+let count = 0;
+while (count < 10) {
+    alert(electricLamp.status);
+    switchButton.switchOn();
+    alert(electricLamp.status);
+    switchButton.switchOff();
+    count++;
+}
